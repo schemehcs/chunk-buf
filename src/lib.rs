ca5c964f8f4b0937b05b6896bfdcc67226afcbb4
schemehcs <94542969+schemehcs@users.noreply.github.com>
2026-06-01 16:25:31 +0800

    add struct Chunk

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,34 @@
-#[derive(Clone)]
+#[derive(PartialEq, Debug)]
+pub struct Chunk<'a> {
+    pub bytes: &'a [u8],
+    pub consumed: usize,
+}
+
+impl<'a> Chunk<'a> {
+    pub fn new(bytes: &'a [u8], consumed: usize) -> Self {
+        Chunk {
+            bytes,
+            consumed
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
 pub struct ChunkBuf {
     buf: Vec<u8>,
-    size: usize,
     cursor: usize,
 }
 
 impl ChunkBuf {
-    pub fn new(size: usize) -> Self {
+    pub fn new(capacity: usize) -> Self {
         Self {
-            buf: vec![0; size],
+            buf: vec![0; capacity],
             cursor: 0,
-            size,
         }
     }
 
-    pub fn update(&mut self, bytes: &[u8]) -> Option<(&[u8], usize)> {
-        let cap = self.size - self.cursor;
+    pub fn update(&mut self, bytes: &[u8]) -> Option<Chunk<'_>> {
+        let cap = self.buf.capacity() - self.cursor;
         if cap > bytes.len() {
             let cursor_n = self.cursor + bytes.len();
             self.buf[self.cursor..cursor_n].copy_from_slice(bytes);
@@ -24,19 +37,20 @@ impl ChunkBuf {
         } else {
             self.buf[self.cursor..].copy_from_slice(&bytes[..cap]);
             self.cursor = 0;
-            Some((&self.buf, cap))
+            Some(Chunk::new(&self.buf, cap))
         }
     }
 
     pub fn remainder(&self) -> &[u8] {
         &self.buf[..self.cursor]
     }
+
 }
 
 impl Drop for ChunkBuf {
     fn drop(&mut self) {
+        // zero buffer to avoid leaving sensitive data on the heap
         self.buf.fill(0);
-        self.cursor = 0;
     }
 }
 
@@ -47,19 +61,11 @@ mod tests {
     #[test]
     fn it_works() {
         let buf1 = [0x1u8, 0x2, 0x3, 0x4];
-        let buf2 = [
-            0x5u8, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf, 0x0, 0xa0, 0xa1, 0xa2, 0xa3,
-        ];
+        let buf2 = [0x5u8, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf, 0x0, 0xa0, 0xa1, 0xa2, 0xa3];
         let mut buf = ChunkBuf::new(8);
         assert_eq!(buf.update(&buf1), None);
-        assert_eq!(
-            buf.update(&buf2),
-            Some((&[0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8][..], 4))
-        );
-        assert_eq!(
-            buf.update(&buf2[4..]),
-            Some((&[0x9, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf, 0x0][..], 8))
-        );
+        assert_eq!(buf.update(&buf2), Some(Chunk::new(&[0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8], 4)));
+        assert_eq!(buf.update(&buf2[4..]), Some(Chunk::new(&[0x9, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf, 0x0], 8)));
         assert_eq!(buf.remainder(), &[][..]);
         assert_eq!(buf.update(&buf2[12..]), None);
         assert_eq!(buf.remainder(), &[0xa0, 0xa1, 0xa2, 0xa3][..]);
